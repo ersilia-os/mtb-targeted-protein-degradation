d4609ec8b4c7bb7d21110f34dba2335ee19f79d5
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2025-01-07 16:04:40 +0100

    updated sequences

--- scripts/00_generate_fasta_files.py
+++ scripts/00_generate_fasta_files.py
@@ -4,7 +4,7 @@ import os
 root = os.path.dirname(os.path.abspath(__file__))
 
 
-def create_fasta(uniprot_id, sequence, output_file, description=""):
+def create_fasta(uniprot_id, sequence, output_file, description="protein|empty"):
     """
     Create a FASTA file with the given UniProt ID and sequence.
 
@@ -14,7 +14,7 @@ def create_fasta(uniprot_id, sequence, output_file, description=""):
     :param description: Optional description for the FASTA header
     """
     # Format the header
-    header = f">{uniprot_id} {description}"
+    header = f">{uniprot_id}|{description}"
     
     # Wrap sequence to 80 characters per line
     wrapped_sequence = "\n".join([sequence[i:i+80] for i in range(0, len(sequence), 80)])
@@ -29,4 +29,4 @@ df = pd.read_csv(os.path.join(root, "..", "data", "mtb_trna_synthetases_bosch_20
 for v in df[["gene_name_in_bosch_2021", "uniprot_ac", "sequence"]].values:
     gene_name, uniprot_id, sequence = v
     output_file = os.path.join(root, "..", "data", "sequences", f"{uniprot_id}.fasta")
-    create_fasta(uniprot_id, sequence, output_file, description=gene_name)
+    create_fasta(uniprot_id, sequence, output_file)
