a6529db8f9dc3b5e0bce265ebfd90e6e93fa8247
Arnau Comajuncosa <arnau.comajuncosa@irbbarcelona.org>
2025-02-23 20:27:43 +0100

    edit pocket detection script

--- scripts/08_detect_pockets.py
+++ scripts/08_detect_pockets.py
@@ -73,11 +73,11 @@ def write_pocket_pdbs(pocket_dict, output_dir):
 
 # Define paths
 root = os.path.dirname(os.path.abspath(__file__))
-aligned_dir = os.path.abspath(os.path.join(root, "..", "processed", "aligned_structures"))
+aligned_dir = os.path.abspath(os.path.join(root, "..", "processed", "aligned_relaxed_structures"))
 detected_pockets_dir = os.path.abspath(os.path.join(root, "..", "processed", "detected_pockets"))
 
 # Load alignment RMSD data
-alignment_df = pd.read_csv(os.path.abspath(os.path.join(root, "..", "processed", "alignment_rmsd_data.csv")))
+alignment_df = pd.read_csv(os.path.abspath(os.path.join(root, "..", "processed", "alignment_relaxed_rmsd_data")))
 
 # Iterate over Uniprot ACs
 for uniprot_ac, file_name in zip(alignment_df["uniprot_ac"], alignment_df["file_name"]):
