d195d271f957e483b8dd0ad781a87cfa91f34f31
Arnau Comajuncosa <arnau.comajuncosa@irbbarcelona.org>
2025-03-10 18:11:04 +0100

    adding coverage in interpro_summary.tsv

--- scripts/10_organize_sequence_info.py
+++ scripts/10_organize_sequence_info.py
@@ -7,6 +7,7 @@ root = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.abspath(os.path.join(root, "..", "data", "sequences", "interpro"))
 outdir = os.path.abspath(os.path.join(root, "..", "processed", "sequences"))
 
+
 # Define uniprots
 uniprots = [i.split(".")[0].split("-")[2] for i in sorted(os.listdir(data_path))]
 uniprots_to_number = {i: c for c,i in enumerate(sorted(uniprots))}
@@ -34,6 +35,9 @@ dfs = pd.concat(dfs, ignore_index=True)
 # Create dict - from accession to name and type
 acc_to_name_type = {i: [j, k] for i,j,k in zip(dfs['Accession'], dfs['Name'], dfs['Type'])}
 
+# Create dict - from accession to protein length and matches
+# acc_to_plength_matches = {i: {} for i in sorted(set(dfs['Accession']))}
+
 interpro_data = []
 interpro_data_collapsed = []  # Do we need it?
 
@@ -43,18 +47,34 @@ for interpro_id in sorted(acc_to_name_type):
     name, type_ = acc_to_name_type[interpro_id]
     proteins = ",".join(dfs[dfs['Accession'] == interpro_id]['Protein Accession'].tolist())
 
+    # For each protein, get coverage
+    coverages = []
+    for protein in proteins.split(","):
+
+        # If len data_protein > 1; error
+        data_protein = dfs[(dfs['Accession'] == interpro_id) & (dfs['Protein Accession'] == protein)]
+        if len(data_protein) != 1:
+            raise Exception("Something did not go as expected. Please check")
+
+        # Get protein length and matches
+        protein_length = int(data_protein["Protein Length"].tolist()[0])
+        matches = data_protein["Matches"].tolist()[0]
+        matches = [i.split("..") for i in matches.split(",")]
+        matches = sum([int(i[1]) - int(i[0]) for i in matches])
+        coverages.append(round(matches / protein_length, 3))
+
     # Create binary vector
     vec = np.zeros(len(uniprots), dtype=int)
     ind = [uniprots_to_number[i] for i in sorted(uniprots_to_number) if i in proteins]
     vec[ind] = 1
 
     # Append to final df
-    cols = [interpro_id, name, type_, len(proteins.split(",")), proteins]
+    cols = [interpro_id, name, type_, len(proteins.split(",")), round(np.mean(coverages), 3), proteins, ",".join([str(i) for i in coverages])]
     cols.extend(vec)
     interpro_data.append(cols)
 
 # Create dataframe
-interpro_data = pd.DataFrame(interpro_data, columns=['Accession', 'Name', 'Type', 'Number of proteins', 'Proteins'] + sorted(uniprots))
+interpro_data = pd.DataFrame(interpro_data, columns=['Accession', 'Name', 'Type', 'Number of proteins', 'Average Coverage', 'Proteins', 'Protein Coverages'] + sorted(uniprots))
 interpro_data = interpro_data.sort_values('Number of proteins', ascending=False).reset_index(drop=True)
 
 # Save results
